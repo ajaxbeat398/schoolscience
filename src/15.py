ff527a37d81f41f8a6fe47d518e7b2c701f4c249
ajaxbeat398 <ajaxbeat398@gmail.com>
2025-03-20 03:11:08 +0000

    Add 15.py

---
+++ src/15.py
@@ -0,0 +1,11 @@
+import random
+
+def random_python_code():
+    # Generate a random number between 1 and 10
+    num = random.randint(1, 10)
+    
+    # Check if the number is even or odd
+    if num % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
